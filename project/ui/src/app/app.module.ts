c261351fc05e60c3d7bbdd4c7485a7478f95c041
ulianoff <unaxim2000@mail.ru>
2019-11-15 22:44:59 +0300

    add new modules and routing

--- project/ui/src/app/app.module.ts
+++ project/ui/src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import {AppComponent} from './app.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {CustomerRegistrationComponent} from './components/customer-registration/customer-registration.component';
+import {FlightsComponent} from './components/flights/flights.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {ExtrasComponent} from './components/extras/extras.component';
+import {ConfirmationComponent} from './components/confirmation/confirmation.component';
+import {RouterModule, Routes} from "@angular/router";
+import { ReviewOrderComponent } from './components/review-order/review-order.component';
+
+const appRoutes: Routes = [
+  {path: 'main', component: LandingComponent},
+  {path:'flight-choose', component: FlightsComponent},
+  {path:'registration', component: CustomerRegistrationComponent},
+  {path:'extras', component: ExtrasComponent},
+  {path:'review-order', component: ReviewOrderComponent},
+  {path:'confirmation', component: ConfirmationComponent},
+  {path:'main/flight-choose', redirectTo:'flight-choose', pathMatch:'full'}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    CustomerRegistrationComponent,
+    FlightsComponent,
+    LandingComponent,
+    ExtrasComponent,
+    ConfirmationComponent,
+    ReviewOrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ project/ui/src/app/components/customer-registration/customer-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fly-customer-registration',
+  templateUrl: './customer-registration.component.html',
+  styleUrls: ['./customer-registration.component.css']
+})
+export class CustomerRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
